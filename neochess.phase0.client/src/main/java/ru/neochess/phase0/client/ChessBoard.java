77e0866f00b52db6c5cdd12ea1fffdd22b68002f
forewar <forewar@gmail.com>
2016-11-02 21:45:27 +0300

    Можно двигать фигуры на доске. Фигуры = объекты Figure.

--- neochess.phase0.client/src/main/java/ru/neochess/phase0/client/ChessBoard.java
+++ neochess.phase0.client/src/main/java/ru/neochess/phase0/client/ChessBoard.java
@@ -1,4 +1,6 @@
-package ru.neochess.phase0.client;import javax.swing.*;
+package ru.neochess.phase0.client;
+
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
@@ -10,8 +12,7 @@ import java.net.URL;
 /**
  * Created by for on 29.10.16.
  */
-class ChessBoard extends JPanel implements ImageObserver, MouseListener, MouseMotionListener
-{
+class ChessBoard extends JPanel implements ImageObserver, MouseListener, MouseMotionListener {
 
     BufferedImage image_buffer;
     ChessServerConnection serverconnection;
@@ -27,7 +28,7 @@ class ChessBoard extends JPanel implements ImageObserver, MouseListener, MouseMo
     private int grabbed_piece, from_row, from_col, to_row, to_col;
     private Figure grabbed_figure;
 
-//    private int chess_matrix[][] = new int[10][10];
+    //    private int chess_matrix[][] = new int[10][10];
 //    private String chess_matrix[][] = new String[3][3];
     private Figure chess_matrix[][] = new Figure[10][10];
 
@@ -63,10 +64,9 @@ M - nothing
     private ImageIcon chessmen_images[] = new ImageIcon[16];
 
 
-    public ChessBoard(ChessClient cc)
-    {
+    public ChessBoard(ChessClient cc) {
         chessclient = cc;
-        this.setSize(500,500);
+        this.setSize(500, 500);
 
         initChessMatrix();
 
@@ -84,15 +84,14 @@ M - nothing
     }
 
     private void initChessMatrix() {
-        for (int i=0; i<10; i++){
-            for(int j=0; j<10; j++){
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
                 chess_matrix[i][j] = null; //fl.getEmptyFigure();
             }
         }
     }
 
-    public void resetBoard()
-    {
+    public void resetBoard() {
         setBoard();
         repaint();
         String encoding = encodeBoard();
@@ -102,8 +101,7 @@ M - nothing
     }
 
 
-    private void setBoard()
-    {
+    private void setBoard() {
 
         decodeBoard("WA1ZZZWA1ZZZWA1WA1WA1ZZZZZZWA1ZZZWA1ZZZWA1");
 
@@ -143,61 +141,51 @@ M - nothing
     }
 
 
-
-    public boolean imageUpdate(Image img, int infoflags, int x, int y, int width, int height)
-    {
+    public boolean imageUpdate(Image img, int infoflags, int x, int y, int width, int height) {
         return true;
     }
 
 
-
-    public void paint(Graphics g)
-    {
+    public void paint(Graphics g) {
         Graphics2D gfx = (Graphics2D) g;
         drawOffscreen();
         gfx.drawImage(image_buffer, 0, 0, this);
     }
 
 
-    private void drawOffscreen()
-    {
+    private void drawOffscreen() {
         Graphics2D gfx = image_buffer.createGraphics();
         renderChessBoard(gfx);
-        if (grabbed_piece != ChessMen.NOTHING)
-            gfx.drawImage(chessmen_images[grabbed_piece].getImage(), x-22, y-22, this);
-
+//        if (grabbed_piece != ChessMen.NOTHING)
+        if (grabbed_figure != null)
+//            gfx.drawImage(chessmen_images[grabbed_piece].getImage(), x - 22, y - 22, this);
+            gfx.drawImage(grabbed_figure.getImage(), x - 22, y - 22, this);
     }
 
 
+    private void renderChessBoard(Graphics2D gfx) {
+        int x = 0, y = 0;
+        boolean b = false;
 
-    private void renderChessBoard(Graphics2D gfx)
-    {
-        int x=0, y=0;
-        boolean b=false;
-
-        for (int i=0; i<10; i++)
-        {
+        for (int i = 0; i < 10; i++) {
             x = 0;
-            b = i%2 == 0;
-            for (int j=0; j<10; j++)
-            {
+            b = i % 2 == 0;
+            for (int j = 0; j < 10; j++) {
                 if (b) gfx.setColor(Color.white);
                 else gfx.setColor(Color.gray);
                 b = !b;
-                gfx.fillRect(x,y,50,50);
+                gfx.fillRect(x, y, 50, 50);
                 paintChessMan(chess_matrix[i][j], x, y, gfx);
                 x += 50;
 
             }
-            y+=50;
+            y += 50;
         }
     }
 
 
-
-//    private void paintChessMan(int piece, int x, int y, Graphics2D gfx)
-    private void paintChessMan(Figure fig, int x, int y, Graphics2D gfx)
-    {
+    //    private void paintChessMan(int piece, int x, int y, Graphics2D gfx)
+    private void paintChessMan(Figure fig, int x, int y, Graphics2D gfx) {
         if (fig != null) {
 
             try {
@@ -211,11 +199,8 @@ M - nothing
     }
 
 
-
-
-    private void CreateChessmenImages()
-    {
-        for (int i=0; i<chessmen_images.length; i++) {
+    private void CreateChessmenImages() {
+        for (int i = 0; i < chessmen_images.length; i++) {
             //URL imageURL = getClass().getResource("/figures/bbishop.gif");
             //System.out.println(imageURL);
             chessmen_images[i] = new ImageIcon(getClass().getResource(chessmen_files[i]));
@@ -223,19 +208,17 @@ M - nothing
     }
 
 
+    public void mouseClicked(MouseEvent e) {
+    }
+
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    public void mouseExited(MouseEvent e) {
+    }
 
 
-    public void mouseClicked(MouseEvent e) { }
-
-    public void mouseEntered(MouseEvent e) { }
-
-    public void mouseExited(MouseEvent e)  { }
-
-
-
-
-    public void mousePressed(MouseEvent e)
-    {
+    public void mousePressed(MouseEvent e) {
         from_row = e.getY() / 50;
         from_col = e.getX() / 50;
 
@@ -259,16 +242,14 @@ M - nothing
     }
 
 
-
-    public void mouseReleased(MouseEvent e)
-    {
-        if (grabbed_piece == ChessMen.NOTHING) return;
+    public void mouseReleased(MouseEvent e) {
+//        if (grabbed_piece == ChessMen.NOTHING) return;
+        if(grabbed_figure == null) return;
 
         to_row = e.getY() / 50;
         to_col = e.getX() / 50;
 
-        if (to_row < 0 || to_row > 9 || to_col < 0 || to_col > 9)
-        {
+        if (to_row < 0 || to_row > 9 || to_col < 0 || to_col > 9) {
             chess_matrix[from_row][from_col] = grabbed_figure; //grabbed_piece;
             //grabbed_piece = ChessMen.NOTHING;
             grabbed_figure = null;
@@ -279,9 +260,8 @@ M - nothing
 
         if ((from_row == to_row && from_col == to_col)
                 ||
-                !isLegalMove(grabbed_piece, from_row, from_col, to_row, to_col))
-        {
-            chess_matrix[from_row][from_col] =  grabbed_figure; //grabbed_piece;
+                !isLegalMove(grabbed_piece, from_row, from_col, to_row, to_col)) {
+            chess_matrix[from_row][from_col] = grabbed_figure; //grabbed_piece;
             //grabbed_piece = ChessMen.NOTHING;
             grabbed_figure = null;
             repaint();
@@ -312,11 +292,9 @@ M - nothing
     }
 
 
-
-    public void mouseDragged(MouseEvent e)
-    {
+    public void mouseDragged(MouseEvent e) {
 //        if (grabbed_piece == ChessMen.NOTHING) return;
-        if(grabbed_figure == null) return;
+        if (grabbed_figure == null) return;
 
         x = e.getX();
         y = e.getY();
@@ -324,37 +302,32 @@ M - nothing
     }
 
 
-
-    public void mouseMoved(MouseEvent e)  { }
-
-
+    public void mouseMoved(MouseEvent e) {
+    }
 
 
-    public int getPieceType(int piece)
-    {
-        switch (piece)
-        {
+    public int getPieceType(int piece) {
+        switch (piece) {
             case ChessMen.WKING:
             case ChessMen.WQUEEN:
             case ChessMen.WROOK:
             case ChessMen.WBISHOP:
             case ChessMen.WKNIGHT:
-            case ChessMen.WPAWN: 	return ChessMen.WHITE;
+            case ChessMen.WPAWN:
+                return ChessMen.WHITE;
             case ChessMen.BKING:
             case ChessMen.BQUEEN:
             case ChessMen.BROOK:
             case ChessMen.BBISHOP:
             case ChessMen.BKNIGHT:
-            case ChessMen.BPAWN: 	return ChessMen.BLACK;
+            case ChessMen.BPAWN:
+                return ChessMen.BLACK;
         }
         return ChessMen.NOTHING;
     }
 
 
-
-
-    boolean isLegalMove(int piece, int from_row, int from_col, int to_row, int to_col)
-    {
+    boolean isLegalMove(int piece, int from_row, int from_col, int to_row, int to_col) {
 
         return true;
 
@@ -373,12 +346,8 @@ M - nothing
     }
 
 
-
-
-    public synchronized void receiveData(String line)
-    {
-        if (line.charAt(0) == '@')
-        {
+    public synchronized void receiveData(String line) {
+        if (line.charAt(0) == '@') {
             processCommand(line);
             return;
         }
@@ -388,60 +357,46 @@ M - nothing
     }
 
 
-
-
-    private void processCommand(String command)
-    {
-        if (command.compareTo("@BLACK") == 0)
-        {
+    private void processCommand(String command) {
+        if (command.compareTo("@BLACK") == 0) {
             myColor = ChessMen.BLACK;
             chessclient.setTitle("Chess Client - BLACK");
             resetBoard();
-        }
-        else
-        if (command.compareTo("@WHITE") == 0)
-        {
+        } else if (command.compareTo("@WHITE") == 0) {
             System.out.println("I am WHITE");
             myColor = ChessMen.WHITE;
             chessclient.setTitle("Chess Client - WHITE");
             resetBoard();
             myTurn = true;
         }
-        if (command.compareTo("@RESET") == 0)
-        {
+        if (command.compareTo("@RESET") == 0) {
             if (myColor == ChessMen.WHITE) myTurn = true;
-        }
-        else
-        if (command.compareTo("@TOKEN") == 0) myTurn = true;
+        } else if (command.compareTo("@TOKEN") == 0) myTurn = true;
     }
 
 
-
-
-	 /*
-	Piece codes:
-	A - wking
-	B - wqueen
-	C - wrook
-	D - wbishop
-	E - wknight
-	F - wpawn
-	G - bking
-	H - bqueen
-	I - brook
-	J - bbishop
-	K - bknight
-	L - bpawn
-	M - nothing
-	*/
-    public String encodeBoard()
-    {
+    /*
+   Piece codes:
+   A - wking
+   B - wqueen
+   C - wrook
+   D - wbishop
+   E - wknight
+   F - wpawn
+   G - bking
+   H - bqueen
+   I - brook
+   J - bbishop
+   K - bknight
+   L - bpawn
+   M - nothing
+   */
+    public String encodeBoard() {
         String encoding = "";
-        for (int i=0; i<10; i++)
-            for (int j=0; j<10; j++)
-            {
+        for (int i = 0; i < 10; i++)
+            for (int j = 0; j < 10; j++) {
 
-                if (chess_matrix[i][j]!=null) {
+                if (chess_matrix[i][j] != null) {
                     encoding += chess_matrix[i][j].encodeFigure();
                 } else {
                     encoding += "ZZZ";
@@ -469,8 +424,7 @@ M - nothing
     }
 
 
-    public void decodeBoard(String encoding)
-    {
+    public void decodeBoard(String encoding) {
         int row, col;
         String pieceRace;
         String pieceCode;
@@ -478,17 +432,16 @@ M - nothing
 
         //if (encoding.length() < 100) return;
 
-        for (int i=0; i<300; i+=3)
-        {
-            if(i>=encoding.length()) return;
+        for (int i = 0; i < 300; i += 3) {
+            if (i >= encoding.length()) return;
 
             row = i / 30;
-            col = (i/3) % 10;
-            pieceRace = encoding.substring(i, i+1); //encoding.charAt(i);
-            pieceCode = encoding.substring(i+1, i+2); //encoding.charAt(i+1);
-            pieceState = encoding.substring(i+2, i+3); //encoding.charAt(i+2);
+            col = (i / 3) % 10;
+            pieceRace = encoding.substring(i, i + 1); //encoding.charAt(i);
+            pieceCode = encoding.substring(i + 1, i + 2); //encoding.charAt(i+1);
+            pieceState = encoding.substring(i + 2, i + 3); //encoding.charAt(i+2);
 
-            if(pieceCode.equalsIgnoreCase("Z")) {
+            if (pieceCode.equalsIgnoreCase("Z")) {
                 chess_matrix[row][col] = null;
             } else {
                 Figure currentFigure = fl.getFigureByCode(pieceCode);
