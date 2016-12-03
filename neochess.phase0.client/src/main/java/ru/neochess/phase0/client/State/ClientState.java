7de581a92f1b0c50bfd7e4958da14389bf36c0ba
Ti Ji <tijifub@gmail.com>
2016-12-03 17:37:45 +0300

    State pattern

---
+++ neochess.phase0.client/src/main/java/ru/neochess/phase0/client/State/ClientState.java
@@ -0,0 +1,7 @@
+package ru.neochess.phase0.client.State;
+
+/**
+ * Created by TiJi on 03.12.16.
+ */
+public interface ClientState {
+}
---
+++ neochess.phase0.client/src/main/java/ru/neochess/phase0/client/State/StateMove.java
@@ -0,0 +1,9 @@
+package ru.neochess.phase0.client.State;
+
+import com.sun.deploy.util.SessionState;
+
+/**
+ * Created by TiJi on 03.12.16.
+ */
+public class StateMove implements ClientState {
+}
